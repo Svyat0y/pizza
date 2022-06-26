4bf902ff22e1b85d745314e50daca048e2586bda
shurik <86realmadrid86@gmail.com>
2022-06-26 21:39:08 +0300

    install react-content-loader and implemented loading block while pizzas are loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import './scss/app.scss'
 import { useEffect, useState } from 'react'
+import axios                   from 'axios'
 
-import axios from 'axios'
-
-import Header     from './components/Header'
-import Categories from './components/Categories'
-import Sort       from './components/Sort'
-import PizzaBlock from './components/PizzaBlock'
+import Header       from './components/Header'
+import Categories   from './components/Categories'
+import Sort         from './components/Sort'
+import PizzaBlock   from './components/PizzaBlock'
+import LoadingBlock from './components/LoadingBlock'
 
 
 function App() {
 	const [ items, setItems ] = useState([])
+	const [ isLoading, setIsLoading ] = useState(false)
 
 	useEffect(() => {
+		setIsLoading(true)
 		axios.get('https://62b869c6f4cb8d63df5d67d3.mockapi.io/pizzas')
-			.then(({ data }) => setItems(data))
-
+			.then(({ data }) => {
+				setTimeout(() => {
+					setItems(data)
+					setIsLoading(false)
+				}, 1000)
+			})
 	}, [])
 
 	return (
@@ -30,7 +36,9 @@ function App() {
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
 						{
-							items.map(obj => <PizzaBlock key={ obj.id } { ...obj }/>)
+							isLoading
+								? Array(10).fill(0).map((_, index) => <LoadingBlock key={ index }/>)
+								: items.map(obj => <PizzaBlock key={ obj.id } { ...obj }/>)
 						}
 					</div>
 				</div>
---
+++ src/components/LoadingBlock.jsx
@@ -0,0 +1,24 @@
+import ContentLoader from 'react-content-loader'
+
+
+const LoadingBlock = () => {
+	return (
+		<ContentLoader
+			className='pizza-block'
+			speed={ 1 }
+			width={ 280 }
+			height={ 457 }
+			viewBox="0 0 280 457"
+			backgroundColor="#f3f3f3"
+			foregroundColor="#ecebeb"
+		>
+			<circle cx="140" cy="140" r="130"/>
+			<rect x="0" y="285" rx="6" ry="6" width="280" height="25"/>
+			<rect x="0" y="325" rx="6" ry="6" width="280" height="84"/>
+			<rect x="0" y="421" rx="6" ry="6" width="92" height="30"/>
+			<rect x="142" y="415" rx="20" ry="20" width="137" height="40"/>
+		</ContentLoader>
+	)
+}
+
+export default LoadingBlock
