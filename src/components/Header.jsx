3b2f8d4d6223c7aa7dc8b8543ecc6f8bcbf03cab
shurik <86realmadrid86@gmail.com>
2022-06-29 20:12:20 +0300

    created Search component and display in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 
 import logoSvg from '../assets/img/pizza-logo.svg'
+import Search  from './Search'
 
 
 const Header = () => {
@@ -16,6 +17,7 @@ const Header = () => {
 						</div>
 					</div>
 				</Link>
+				<Search/>
 				<div className="header__cart">
 					<Link to={ '/cart' } className="button button--cart">
 						<span>520 ₽</span>
