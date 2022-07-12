f41a41dbd25be678a64ec907dbc5d8902aa92bec
shurik <86realmadrid86@gmail.com>
2022-07-12 21:31:24 +0300

    added tsconfig and renamed 2 files

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import { createRoot }    from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import { Provider } from 'react-redux'
+import { store }    from './redux/store'
+
+import App from './App'
+
+
+const rootElem = document.getElementById('root')
+
+if (rootElem) {
+	const root = createRoot(rootElem)
+	root.render(
+		<Provider store={ store }>
+			<BrowserRouter>
+				<App/>
+			</BrowserRouter>
+		</Provider>
+	)
+}
